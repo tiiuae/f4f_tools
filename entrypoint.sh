655ac3fd391e96b8af8cdd254b7abc4ea89e292c
Daniel Smrcka <smrckda1@fel.cvut.cz>
2022-07-18 16:05:38 +0200

    Moved scripts to alias

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source /opt/ros/galactic/setup.bash
 
+echo "alias land=\"ros2 service call /"$DRONE_DEVICE_ID"/control_interface/land std_srvs/srv/Trigger\"" >> ~/.bashrc
+echo "alias takeoff=\"ros2 service call /"$DRONE_DEVICE_ID"/control_interface/takeoff std_srvs/srv/Trigger\"" >> ~/.bashrc
+
+
 bash
